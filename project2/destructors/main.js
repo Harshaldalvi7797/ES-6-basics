28dfccd3a5c4c2442c4710996984144d3b908f50
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-04-24 12:17:49 +0530

    commit : arrray and object destructuring

--- project2/destructors/main.js
+++ project2/destructors/main.js
@@ -25,3 +25,50 @@ for (let userModelObj of userModel) {
 }
 let [userModel1, userModel2, userModel3] = userModel;
 console.log(`${userModel1.firstname} ${userModel1.lastname}`);
+
+//object desctructors
+
+let userModel5Obj = {
+  firstname1: "Mayur",
+  lastname: "Veer"
+};
+console.log(userModel5Obj.firstname1);
+let { firstname1, lastname1 } = userModel5Obj;
+console.log(firstname1);
+
+let userModel6Obj = {
+  firstname: "Harshal",
+  lastname: "Dalvi",
+  communication: {
+    email1: "h@gmail.com",
+    mno1: "8087737437"
+  }
+};
+let {
+  firstname,
+  lastname,
+  communication: { email1, mno1 }
+} = userModel6Obj;
+console.log(mno1);
+console.log(email1);
+console.log(userModel6Obj.communication);
+
+let userModel7Obj = {
+  firstname: "Harshal",
+  lastname: "Dalvi",
+  communication: [
+    {
+      email: "h@gmail.com",
+      mno: "8087737437"
+    },
+    {
+      email: "h1@gmail.com",
+      mno: "7039271020"
+    }
+  ]
+};
+let {
+  communication: [{ email, mno }]
+} = userModel7Obj;
+console.log(userModel7Obj.communication[0]);
+console.log(userModel7Obj.communication[1]);
